4c73627a0019651be254f24a1fa5e2fd8eda4cd7
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-06-07 16:28:21 -0300

    add tailwind and daisyui

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,5 @@
 import { hydrate, prerender as ssr } from "preact-iso";
+import { useEffect, useState } from "preact/hooks";
 
 import "./style.css";
 
@@ -17,13 +18,45 @@ function DailySmiles({ daySmiles }) {
   ));
 }
 
+function ThemeSelector() {
+  const [theme, setTheme] = useState(document.body.getAttribute("data-theme"));
+
+  useEffect(() => {
+    document.body.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  return (
+    <div class="flex gap-3">
+      <div
+        class={`btn btn-sm ${theme === "dracula" ? "btn-primary" : ""}`}
+        onClick={() => setTheme("dracula")}
+      >
+        Dracula
+      </div>
+      <div
+        class={`btn btn-sm ${theme === "dark" ? "btn-primary" : ""}`}
+        onClick={() => setTheme("dark")}
+      >
+        Dark
+      </div>
+      <div
+        class={`btn btn-sm ${theme === "light" ? "btn-primary" : ""}`}
+        onClick={() => setTheme("light")}
+      >
+        Light
+      </div>
+    </div>
+  );
+}
+
 export function App() {
   return (
     <article class="prose lg:prose-lg">
-      <header>
-        <a href="/">
+      <header class="flex justify-between items-center">
+        <a class="underline-offset-8 border-b-2 border-b-black" href="/">
           <span>Sorriso do Dia</span>
         </a>
+        <ThemeSelector />
       </header>
       <main>
         <h2>Todo dia um motivo novo pra sorrir! :D</h2>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+import daisyui from "daisyui";
+import twTypography from "@tailwindcss/typography";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["index.html", "./src/**/*.{html,jsx,tsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [daisyui, twTypography],
+  daisyui: {
+    themes: ["dracula", "dark", "light"],
+  },
 };
